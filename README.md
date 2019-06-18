defab7a1b8e6f82b8ad1c40275bf195711a85664
Viktor Kirillov <mr.kirillov@icloud.com>
2019-06-18 12:15:08 -0500

    Upload

---
+++ README.md
@@ -0,0 +1,62 @@
+# ZyBooks Ultimate Hack
+
+[![HitCount](http://hits.dwyl.io/kirillovmr/zybooks-hack.svg)](http://hits.dwyl.io/kirillovmr/zybooks-hack)
+
+![ZyBooks logo](https://learn.zybooks.com/zycommon-web/assets/logo.svg "ZyBooks logo")
+
+Because that [ZyBooks](https://www.zybooks.com/) uses its client part to check whether assognment is correct or no, I found an ability to intercept and modify outgoing requests. Therefore, all ZyBooks assignment (except those are checked manually) could be passed with this extension.
+
+
+## Features
+
+1. Press the extension icon to enable / disable requests interception.
+2. Explicit notification to keep track of hacking progress.
+3. Passing participation and challange activities.
+
+
+## Installation
+This is a chrome extension, it is not published to Chrome WebStore, therefore you need to install it manually.
+
+Download build *[Not yet]*:
+Download build_latest.zip, extract, go to **Chrome -> Extensions**. Make sure the **Developer Mode** is *ON*.
+Press **Load unpacked** -> Select the build folder on your machine with *manifest.json* file inside.
+
+**OR**
+
+Download project and compile:
+Download [this repo](), extract, open the terminal window in project directory, run
+```js
+npm run build
+```
+Then go to **Chrome -> Extensions**. Make sure the **Developer Mode** is *ON*.
+Press **Load unpacked** -> Select the build folder on your machine with *manifest.json* file inside.
+
+
+## Usage
+When the extension is enabled, ZyBooks Hack is ready to intercept.
+To pass Participation activities (multiple choice questions):
+  1. **Select any answer or** if there is a button "show answer" - **press "show answer" twice**.
+  2. **You will see the notification** about the progress.
+  3. When the progress is done - **reload the page** to see the results.
+
+To pass Challange activities (coding part):
+  1. Press **Run** button.
+  2. After standard tests passed, Hack will start and you will see the notification with its progress.
+  3. When the progress is done - **reload the page** to see the results.
+
+:bulb: Trick: You can proceed to the next question when the hack progress done without reloading the page. After all the activities, reload the page andd all of them will be passed.
+
+## Warning
+:bangbang: Project was made for test purposes only in order to advance website and network security.
+All actions you take at your own risk. The author is not responsible for the consequences of your actions.
+
+
+## Contributing
+
+1. **Please!! Do not create a pull request without an issue before discussing the problem.**
+2. On your PR make sure that you are following the current codebase style.
+3. Your PR must be single purpose. Resolve just one problem on your PR.
+4. Make sure to commit in the same style that we are committing until now on the project.
+
+-------------
+Viktor Kirillov ~ [@kirillovmr](https://github.com/kirillovmr)
---
+++ src/js/background.js
@@ -0,0 +1,152 @@
+import logo from "../img/zybooks-logo.svg"
+
+// Import images for build
+import i34_enabled from "../img/icon34-01.png"
+import i34_disabled from "../img/icon34-02.png"
+import i128 from "../img/icon128-01.png"
+
+// Settings for custom notifications
+const initNotification = () => chrome.notifications.create('initialize', {
+    type: "basic",
+    title: "ZyBooks Hack",
+    message: "ZyBooks Hack was initialized successfully.",
+    iconUrl: logo,
+}, () => setTimeout(() => chrome.notifications.clear("initialize", null), 2500));
+const disableNotification = () => chrome.notifications.create('disable', {
+    type: "basic",
+    title: "ZyBooks Disabled",
+    message: "ZyBooks Hack was disabled.",
+    iconUrl: logo,
+}, () => setTimeout(() => chrome.notifications.clear("disable", null), 2500));
+const successNotification = () => chrome.notifications.create('success', {
+    type: "basic",
+    title: "Task hacked :)",
+    message: "Task was successfully hacked!\nReload page to see the results.",
+    iconUrl: logo,
+}, () => setTimeout(() => chrome.notifications.clear("success", null), 2500));
+
+var progressNotif = {
+    type: "progress",
+    title: "Processing...",
+    message: "Hacking in progress...",
+    iconUrl: logo,
+};
+var iterErrorNotif = {
+    type: "basic",
+    title: "Error occured",
+    message: "Error occured on iteration 1",
+    iconUrl: logo,
+};
+
+// General settings
+const reqInitiator = "https://learn.zybooks.com";
+const urlPattern = "*://*.zybooks.com/*";
+let state = {
+    enabled: true
+};
+
+const setIcon = (state) => chrome.browserAction.setIcon({path: state ? "icon34-01.png": "icon34-02.png"});
+
+// Getting initial state from storage
+chrome.storage.sync.get(['enabled'], function(result) {
+    console.log('Local storage:', result);
+    state.enabled = result.enabled
+    setIcon(state.enabled);
+});
+
+// Handling clicking on extension icon event
+chrome.browserAction.onClicked.addListener((tab) => {
+    state.enabled = !state.enabled;
+    setIcon(state.enabled);
+
+    // Saving state in local storage
+    chrome.storage.sync.set({enabled: state.enabled}, function() {
+        state.enabled ? initNotification() : disableNotification();
+        console.log(`ZyBooks Hack was ${state.enabled ? 'enabled' : 'disabled'}.`);
+    });
+});
+
+// Creating initial notification
+// initNotification()
+console.log("ZyBooks hack loaded successfully!");
+
+// Event handlers for outgoing requests
+(function() {
+    let payload = null;
+
+    function send_req(headers, body, iteration) {
+        fetch(headers.url, {  
+            "credentials": "omit",
+            "headers": { 
+                "accept":" application/json, text/javascript, */*; q=0.01",
+                "Content-type": "application/json"
+            },
+            "referrer":"https://learn.zybooks.com/zybook/UICCS141HallenbeckSummer2019/chapter/1/section/2?content_resource_id=32105341",
+            "referrerPolicy":"no-referrer-when-downgrade",
+            "method": "POST",
+            "mode": "cors",
+            "body": body
+        })
+        .then((r) => {
+            chrome.notifications.update('request', {...progressNotif, progress: (iteration+1)*10}, null);
+            console.log(`Iteration ${iteration} successfully sended.`);
+        })
+        .catch((e) => {
+            chrome.notifications.create('iterError', {...iterErrorNotif, message: `Error occured on iteration ${iteration}`}, () => setTimeout(() => chrome.notifications.clear("iterError", null), 4000));
+            console.warn(`Iteration ${iteration} ended with error, ${e}`);
+        })
+    }
+
+    chrome.webRequest.onBeforeRequest.addListener((details) => {
+        if (!state.enabled) return;
+
+        if (details.initiator == reqInitiator) {
+            if (details.url.split('/').reverse()[0] == "activity") {
+                if (details.requestBody) {
+                    // Decoding payload
+                    var postedString = decodeURIComponent(String.fromCharCode.apply(null,
+                        new Uint8Array(details.requestBody.raw[0].bytes)));
+                    // console.log('REQ', details);
+                    let json = JSON.parse(postedString);
+
+                    // Saving payload
+                    payload = json;
+                }
+            }
+        }
+    }, {urls: [urlPattern]},
+    ["requestBody"]);
+
+    chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+        if (!state.enabled) return;
+
+        if (details.initiator == reqInitiator) {
+            if (details.url.split('/').reverse()[0] == "activity") {
+                // Saving required headers
+                // console.log("Headers:", details)
+                let savedHeaders = {
+                    url: details.url,
+                    headers: details.requestHeaders
+                };
+
+                // Delayed loop
+                chrome.notifications.create('request', {...progressNotif, progress: 0}, null);
+                var i = 0, howManyTimes = 10;
+                function f() {
+                    // Sending requests
+                    send_req(savedHeaders, JSON.stringify({...payload, part: i, complete: true}), i);
+                    i++;
+                    if( i < howManyTimes ){
+                        setTimeout( f, 500 );
+                    } else {
+                        chrome.notifications.clear('request', null);
+                        successNotification();
+                    }
+                }
+                f();
+            }
+        }
+    },
+    {urls: [urlPattern]},
+    ["requestHeaders"]);
+}());
